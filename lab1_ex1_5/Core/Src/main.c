8f136d9e075e74669135e9c5302322e20f7fb48b
dinkmsd <85824110+dinkmsd@users.noreply.github.com>
2022-09-22 11:26:27 +0700

    fix some bug

--- lab1_ex1_5/Core/Src/main.c
+++ lab1_ex1_5/Core/Src/main.c
@@ -86,7 +86,12 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-
+  void display7SEG (int num){
+  	char led7seg[10] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
+  	for (int i = 0; i < 7; i++){
+  		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0 << i, (led7seg[num] >> i) & 1);
+  	}
+  }
   /* USER CODE END 2 */
 
   /* Infinite loop */
